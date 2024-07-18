3d064762d6b0701e4ef9e3adfbb1208d4a6ce4f3
Your Name <you@example.com>
2024-07-18 00:00:46 -0400

    DC

--- src/app/(layout)/(layout-admin)/Admin/Tarifas/page.jsx
+++ src/app/(layout)/(layout-admin)/Admin/Tarifas/page.jsx
@@ -190,18 +190,18 @@ export default function Home() {
 
   async function getExchage(i) {
     const headers = {
-      "Accept": "*/*",
-      "Accept-Encoding": "gzip, deflate, br",
-      "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
-      "Cache-Control": "no-cache",
-      "Connection": "keep-alive",
+      // "Accept": "*/*",
+      // "Accept-Encoding": "gzip, deflate, br",
+      // "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+      // "Cache-Control": "no-cache",
+      // "Connection": "keep-alive",
       "Content-Length": "123",
       "content-type": "application/json",
-      "Host": "p2p.binance.com",
-      "Origin": "https://p2p.binance.com",
-      "Pragma": "no-cache",
-      "TE": "Trailers",
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
+      // "Host": "p2p.binance.com",
+      // "Origin": "https://p2p.binance.com",
+      // "Pragma": "no-cache",
+      // "TE": "Trailers",
+      // "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
     };
 
     const data = {
