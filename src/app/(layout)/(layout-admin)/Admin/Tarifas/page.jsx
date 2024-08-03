062e5f25b0f5163c5c22a4104e7e3c09bd79f2cb
Your Name <you@example.com>
2024-08-02 22:27:20 -0400

    DC

--- src/app/(layout)/(layout-admin)/Admin/Tarifas/page.jsx
+++ src/app/(layout)/(layout-admin)/Admin/Tarifas/page.jsx
@@ -212,7 +212,7 @@ export default function Home() {
   //     filterType: 'all'
   //   };
 
-    
+
   //   const responseData = await fetch(
   //     'https://cors-anywhere.herokuapp.com/https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search',
   //     {
@@ -247,8 +247,8 @@ export default function Home() {
 
 
 
-async function getChangeP2P (e) {
-  e.preventDefault()
+  async function getChangeP2P(e) {
+    e.preventDefault()
 
     const data = {
       asset: crypto,
@@ -262,69 +262,72 @@ async function getChangeP2P (e) {
     };
 
 
- let res= await fetch('/api/getP2P',    {
-  headers: {      
-    "Content-Length": "123",
-    "content-type": "application/json",},
-  method: 'POST',
-  body: JSON.stringify({ ...data }),
+    let res = await fetch('/api/getP2P', {
+      headers: {
+        "Content-Length": "123",
+        "content-type": "application/json",
+      },
+      method: 'POST',
+      body: JSON.stringify({ ...data }),
 
-})
- let resDB =await res.json()
- console.log()
-   setResP2P(resDB.data)
+    })
+    let resDB = await res.json()
+    console.log()
+    setResP2P(resDB.data)
 
-}
+  }
+
+
+
+
+
+
+
+
 
 
 
   async function getExchage(i) {
-    const headers = {
-      // "Accept": "*/*",
-      // "Accept-Encoding": "gzip, deflate, br",
-      // "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
-      // "Cache-Control": "no-cache",
-      // "Connection": "keep-alive",
-      "Content-Length": "123",
-      "content-type": "application/json",
-      // "Host": "p2p.binance.com",
-      // "Origin": "https://p2p.binance.com",
-      // "Pragma": "no-cache",
-      // "TE": "Trailers",
-      // "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
-    };
 
     const data = {
       asset: 'USDT',
       // tradeType: 'BUY',
       fiat: i.code,
-      transAmount: state[i.code] && state[i.code].transAmount ? state[i.code].transAmount : (i.transAmount ? i.transAmount:  0),
+      transAmount: state[i.code] && state[i.code].transAmount ? state[i.code].transAmount : (i.transAmount ? i.transAmount : 0),
       order: '',
       page: 1,
       rows: 5,
       filterType: 'all'
     };
 
-    const responseData = await fetch(
-      'https://cors-anywhere.herokuapp.com/https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search',
-      {
-        headers,
-        method: 'POST',
-        body: JSON.stringify({ ...data, tradeType: 'BUY', }),
 
-      }
-    );
-    const responseData2 = await fetch(
-      'https://cors-anywhere.herokuapp.com/https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search',
-      {
-        headers,
-        method: 'POST',
-        body: JSON.stringify({ ...data, tradeType: 'SELL', }),
+    let responseData = await fetch('/api/getUsdtP2P', {
+      headers: {
+        "Content-Length": "123",
+        "content-type": "application/json",
+      },
+      method: 'POST',
+      body: JSON.stringify({ ...data, tradeType: 'BUY', }),
 
-      }
-    );
-    // if (!responseData.ok) throw 'bad response';
-    // if (!responseData.ok) throw 'bad response';
+    })
+
+
+    let responseData2 = await fetch('/api/getUsdtP2P', {
+      headers: {
+        "Content-Length": "123",
+        "content-type": "application/json",
+      },
+      method: 'POST',
+      body: JSON.stringify({ ...data, tradeType: 'BUY', }),
+
+    })
+
+
+
+
+
+
+
 
     const jsonData = await responseData.json();
     const jsonData2 = await responseData2.json();
@@ -338,13 +341,87 @@ async function getChangeP2P (e) {
       let tempMaxima2 = Math.max(...jsonData2.data.map((i) => i.adv.price));
       let tempMinima2 = Math.min(...jsonData2.data.map((i) => i.adv.price));
       let promedio2 = (tempMaxima2 + tempMinima2) / 2;
-      setState({ ...state, [i.code]: { ...state[i.code],  compra: (promedio2 + 0.01).toFixed(2), venta: (promedio + 0.01).toFixed(2) } })
+      setState({ ...state, [i.code]: { ...state[i.code], compra: (promedio2 + 0.01).toFixed(2), venta: (promedio + 0.01).toFixed(2) } })
     } else {
       setModal('NonExchange')
       setItem({ ...i, transAmount: state[i.code] && state[i.code].transAmount ? state[i.code].transAmount : 0, })
     }
 
   }
+
+
+
+
+
+
+
+  // async function getExchage(i) {
+  //   const headers = {
+  //     // "Accept": "*/*",
+  //     // "Accept-Encoding": "gzip, deflate, br",
+  //     // "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+  //     // "Cache-Control": "no-cache",
+  //     // "Connection": "keep-alive",
+  //     "Content-Length": "123",
+  //     "content-type": "application/json",
+  //     // "Host": "p2p.binance.com",
+  //     // "Origin": "https://p2p.binance.com",
+  //     // "Pragma": "no-cache",
+  //     // "TE": "Trailers",
+  //     // "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
+  //   };
+
+  //   const data = {
+  //     asset: 'USDT',
+  //     // tradeType: 'BUY',
+  //     fiat: i.code,
+  //     transAmount: state[i.code] && state[i.code].transAmount ? state[i.code].transAmount : (i.transAmount ? i.transAmount : 0),
+  //     order: '',
+  //     page: 1,
+  //     rows: 5,
+  //     filterType: 'all'
+  //   };
+
+  //   const responseData = await fetch(
+  //     'https://cors-anywhere.herokuapp.com/https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search',
+  //     {
+  //       headers,
+  //       method: 'POST',
+  //       body: JSON.stringify({ ...data, tradeType: 'BUY', }),
+
+  //     }
+  //   );
+  //   const responseData2 = await fetch(
+  //     'https://cors-anywhere.herokuapp.com/https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search',
+  //     {
+  //       headers,
+  //       method: 'POST',
+  //       body: JSON.stringify({ ...data, tradeType: 'SELL', }),
+
+  //     }
+  //   );
+  //   // if (!responseData.ok) throw 'bad response';
+  //   // if (!responseData.ok) throw 'bad response';
+
+  //   const jsonData = await responseData.json();
+  //   const jsonData2 = await responseData2.json();
+
+  //   if (jsonData.data.length !== 0 && jsonData2.data.length !== 0) {
+  //     let tempMaxima = Math.max(...jsonData.data.map((i) => i.adv.price));
+  //     let tempMinima = Math.min(...jsonData.data.map((i) => i.adv.price));
+  //     let promedio = (tempMaxima + tempMinima) / 2;
+
+
+  //     let tempMaxima2 = Math.max(...jsonData2.data.map((i) => i.adv.price));
+  //     let tempMinima2 = Math.min(...jsonData2.data.map((i) => i.adv.price));
+  //     let promedio2 = (tempMaxima2 + tempMinima2) / 2;
+  //     setState({ ...state, [i.code]: { ...state[i.code], compra: (promedio2 + 0.01).toFixed(2), venta: (promedio + 0.01).toFixed(2) } })
+  //   } else {
+  //     setModal('NonExchange')
+  //     setItem({ ...i, transAmount: state[i.code] && state[i.code].transAmount ? state[i.code].transAmount : 0, })
+  //   }
+
+  // }
 
   // async function getExchage(i) {
   //   const headers = {
